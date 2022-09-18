3b5597058e9a6f5673cfbf27b2105ab8ac773d7f
AdrielModollo <adrielmodollo@outlook.com>
2022-09-17 23:31:15 -0300

    feat: implements methods interface

---
+++ src/main/java/br/com/adriel/video.java
@@ -0,0 +1,20 @@
+package br.com.adriel;
+
+public class video implements AcoesVideo {
+
+	@Override
+	public void play() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void like() {
+
+	}
+
+}
