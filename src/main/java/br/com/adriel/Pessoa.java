7d458c39feded5f1d9c7904b45fd0a503ca355c5
AdrielModollo <adrielmodollo@outlook.com>
2022-09-17 23:47:21 -0300

    feat: class abstract pessoa

---
+++ src/main/java/br/com/adriel/Pessoa.java
@@ -0,0 +1,35 @@
+package br.com.adriel;
+
+public abstract class Pessoa {
+	protected String nome;
+	protected int idade;
+	protected String sexo;
+	protected float experience;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public float getExperience() {
+		return experience;
+	}
+	public void setExperience(float experience) {
+		this.experience = experience;
+	}
+	
+		
+}
