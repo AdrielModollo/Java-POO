41c5b5d1a4754b0891503daf3be23d30fcf90216
AdrielModollo <adrielmodollo@outlook.com>
2022-09-17 23:39:11 -0300

    feat: prepare to receveid video vector

--- src/main/java/br/com/adriel/Startup.java
+++ src/main/java/br/com/adriel/Startup.java
@@ -1,13 +1,10 @@
 package br.com.adriel;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Startup {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
+		Video v[] = new Video[3];
+		v[0] = new Video("O Senhor dos Anéis");
+		v[1] = new Video("Vingadores");
+		v[2] = new Video("Spider Man");
 	}
-
 }
