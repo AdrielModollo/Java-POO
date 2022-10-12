489daee60b3ed8e3dfbe8c30a09fe8e80ae6caae
AdrielModollo <adrielmodollo@outlook.com>
2022-10-11 22:46:27 -0300

    feat: add constructor

--- src/main/java/br/com/adriel/Usuario.java
+++ src/main/java/br/com/adriel/Usuario.java
@@ -4,6 +4,10 @@ public class Usuario extends Pessoa {
 	private String login;
 	private int totalViews;
 	
+	public Usuario(String nome, int idade, String sexo, String login) {
+		super(nome, idade, sexo);
+	}
+	
 	public String getLogin() {
 		return login;
 	}
