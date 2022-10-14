31afd7f274a19611993e905eb4d179c8bcb085da
AdrielModollo <adrielmodollo@outlook.com>
2022-10-13 23:47:14 -0300

    feat: generate getters and setters

--- src/main/java/br/com/adriel/Visualizacao.java
+++ src/main/java/br/com/adriel/Visualizacao.java
@@ -4,5 +4,18 @@ public class Visualizacao {
 	private Usuario espectador;
 	private Video filme;
 	
+	public Usuario getEspectador() {
+		return espectador;
+	}
+	public void setEspectador(Usuario espectador) {
+		this.espectador = espectador;
+	}
+	public Video getFilme() {
+		return filme;
+	}
+	public void setFilme(Video filme) {
+		this.filme = filme;
+	}
+	
 	
 }
